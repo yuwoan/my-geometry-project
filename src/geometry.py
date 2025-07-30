af736025cbcf34de56e1e0b9060f85a6f5876831
Weiyi Gao <weiyi.gao@Joynext.com>
2025-07-30 10:36:49 +0800

    Add perimeter calculation to Circle class

--- src/geometry.py
+++ src/geometry.py
@@ -6,6 +6,9 @@ class Circle:
 
     def area(self):
         return math.pi * (self.radius ** 2)
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
 
 class Rectangle:
     def __init__(self, width, height):
