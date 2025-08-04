3a270fa9b4f4bbe98773cdeb2063376004e70187
Weiyi Gao <weiyi.gao@Joynext.com>
2025-08-04 10:30:14 +0800

    保留两位小数

--- src/geometry.py
+++ src/geometry.py
@@ -7,7 +7,7 @@ class Circle:
         self.radius = radius
 
     def area(self):
-        return math.pi * (self.radius ** 2)
+        return round(math.pi * (self.radius ** 2), 2)
     
     def perimeter(self):
         return 2 * math.pi * self.radius
